65d66229773a7739cc53077cf4d421b41d60f48e
Circuare <oncwnuB6VGpwQmtQ6moTOH0tiLDg@git.weixin.qq.com>
2024-03-11 17:59:43 +0800

    evaluation界面优化

--- pages/evaluation/evaluation.js
+++ pages/evaluation/evaluation.js
@@ -6,13 +6,33 @@ Page({
     searchResult: [],
     scaleList: [],
     tags: ['全部'],
-    selectedTag: '全部'
+    selectedTag: '全部',
+    backgroundColorMap: {
+      '抑郁': '#FFB6C1',
+      '焦虑': '#87CEFA',
+      '社交': '#FFA500',
+      '孤独': '#9370DB',
+      '双相': '#00CED1',
+      '儿童': '#90EE90',
+      '患者': '#90EE90',
+      '老年': '#90EE90',
+      '考试': '#FA8072',
+      '产后': '#FA8072',
+    }
   },
   onLoad: function () {
     db.collection('scale').get({
       success: res => {
         const searchResult = res.data;
-        const tags = [...new Set(searchResult.map(item => item.tag))];
+        const tagCountMap = {};
+        searchResult.forEach(item => {
+          item.tag.forEach(tag => {
+            tagCountMap[tag] = (tagCountMap[tag] || 0) + 1;
+          });
+        });
+        const tagCountArray = Object.entries(tagCountMap);
+        tagCountArray.sort((a, b) => b[1] - a[1]);
+        const tags = tagCountArray.map(entry => entry[0]);
         this.setData({
           searchResult: res.data,
           scaleList: searchResult,
@@ -28,15 +48,25 @@ Page({
 
   onInputChange: function (event) {
     const searchText = event.detail.value;
+    this.setData({
+      noResultMessage: ''
+    });
     db.collection('scale').where({
       title: db.RegExp({
         regexp: searchText,
         options: 'i'
       })
     }).get().then(res => {
-      this.setData({
-        searchResult: res.data
-      });
+      if (res.data.length > 0) {
+        this.setData({
+          searchResult: res.data
+        });
+      } else {
+        this.setData({
+          searchResult: [], 
+          noResultMessage: '没有找到你想找的内容哦~请换个词试试！'
+        });
+      }
     }).catch(err => {
       console.error('搜索失败：', err);
     });
