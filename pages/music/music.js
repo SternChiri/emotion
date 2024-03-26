ff92ab24fd27b8bf54aed15c5adb4842044dc695
18159258695 <oncwnuGYKt2gpmdoIQfskF7C3w9s@git.weixin.qq.com>
2024-03-26 23:03:15 +0800

    歌能听了

--- pages/music/music.js
+++ pages/music/music.js
@@ -1,20 +1,60 @@
+const db = wx.cloud.database();
+
 Page({
   data: {
-    musicUrl: 'cloud://your-cloud-storage-bucket/music.mp3',
+    musics: [],
+    innerAudioContext: null, 
+    currentMusic: {
+      name: '',
+      url: '',
+    }
   },
-
-  playMusic: function() {
-    const audio = this.selectComponent('.music-player audio');
-    audio.play();
+  onLoad: function () {
+    this.getMusics();
+    this.setData({
+      innerAudioContext: wx.createInnerAudioContext(), // 在页面加载时创建 InnerAudioContext 对象
+    });
   },
-
-  pauseMusic: function() {
-    const audio = this.selectComponent('.music-player audio');
-    audio.pause();
+  getMusics: function () {
+    db.collection('music').get({
+      success: res => {
+        this.setData({
+          musics: res.data
+        });
+      },
+      fail: err => {
+        console.error('获取音乐数据失败：', err);
+      }
+    });
   },
-
-  stopMusic: function() {
-    const audio = this.selectComponent('.music-player audio');
-    audio.stop();
+  playMusic: function (event) {
+    const musicId = event.currentTarget.dataset.id;
+    const music = this.data.musics.find(item => item._id === musicId);
+    if (music) {
+      const innerAudioContext = this.data.innerAudioContext;
+      if (this.data.innerAudioContext) {
+        innerAudioContext.stop(); 
+      }
+      innerAudioContext.src = music.musicUrl;
+      innerAudioContext.play();
+      this.setData({
+        currentMusic: { 
+          name: music.name,
+          url: music.musicUrl,
+        },
+      });
+    } else {
+      console.error('未找到对应的歌曲信息');
+    }
   },
+  pauseMusic: function () {
+    const innerAudioContext = this.data.innerAudioContext;
+    innerAudioContext.pause();
+  },
+  resumeMusic: function () {
+    const innerAudioContext = this.data.innerAudioContext;
+    innerAudioContext.play();
+  
+  },
+  
 });
