ea0872bf823519a032070f9039809e7af3d61dc0
18159258695 <oncwnuGYKt2gpmdoIQfskF7C3w9s@git.weixin.qq.com>
2024-03-28 21:26:18 +0800

    超

--- pages/meditation/meditation.js
+++ pages/meditation/meditation.js
@@ -1,48 +1,110 @@
+const db = wx.cloud.database();
 Page({
   data: {
-    musicUrl: 'cloud://your-cloud-storage-bucket/meditation.mp3',
-    isPlaying: false,
-    currentTime: 0,
-    duration: 0,
+    musics: [],
+    innerAudioContext: null,
+    currentMusic: {
+      name: '',
+      url: '',
+      isPlaying: false
+    },
+    currentIndex: 0
   },
-
-  onLoad: function() {
-    const audio = this.selectComponent('.meditation-player audio');
-    audio.onCanplay(() => {
-      this.setData({
-        duration: audio.duration,
+  onLoad: function () {
+    this.getMusics();
+    this.setData({
+      innerAudioContext: wx.createInnerAudioContext()
+    });
+  },
+  getMusics: function () {
+    db.collection('meditation').get({
+      success: res => {
+        this.setData({
+          musics: res.data
+        });
+      },
+      fail: err => {
+        console.error('获取音乐数据失败：', err);
+      }
+    });
+  },
+  onUnload: function () {
+    const innerAudioContext = this.data.innerAudioContext;
+    if (innerAudioContext) {
+      innerAudioContext.stop();
+      innerAudioContext.destroy();
+    }
+  },
+  playMusic: function (event) {
+    const musicId = event.currentTarget.dataset.id;
+    const meditation = this.data.musics.find(item => item._id === musicId);
+    if (meditation) {
+      const innerAudioContext = this.data.innerAudioContext;
+      innerAudioContext.pause();
+      innerAudioContext.src = meditation.musicUrl;
+      innerAudioContext.onCanplay(() => {
+        innerAudioContext.play();
+        this.setData({
+          currentMusic: {
+            name: meditation.name,
+            url: meditation.musicUrl,
+            isPlaying: true
+          },
+          currentIndex: this.data.musics.findIndex(item => item._id === musicId)
+        });
       });
-    });
+    } else {
+      console.error('未找到对应的歌曲信息');
+    }
   },
-
-  playMusic: function() {
-    const audio = this.selectComponent('.meditation-player audio');
-    audio.play();
+  togglePlayPause: function () {
+    const innerAudioContext = this.data.innerAudioContext;
+    const currentMusic = this.data.currentMusic;
+    if (currentMusic.isPlaying) {
+      innerAudioContext.pause();
+    } else {
+      innerAudioContext.play();
+    }
     this.setData({
-      isPlaying: true,
+      'currentMusic.isPlaying': !currentMusic.isPlaying,
     });
   },
+  prevMusic: function () {
+    let index = this.data.currentIndex - 1;
+    if (index < 0) {
+      index = this.data.musics.length - 1;
+    }
+    const meditation = this.data.musics[index];
+    this.playMusicById(meditation._id);
+  },
+  nextMusic: function () {
+    let index = this.data.currentIndex + 1;
+    if (index >= this.data.musics.length) {
+      index = 0;
+    }
+    const meditation = this.data.musics[index];
+    this.playMusicById(meditation._id);
 
-  pauseMusic: function() {
-    const audio = this.selectComponent('.meditation-player audio');
-    audio.pause();
-    this.setData({
-      isPlaying: false,
-    });
   },
-
-  stopMusic: function() {
-    const audio = this.selectComponent('.meditation-player audio');
-    audio.stop();
-    this.setData({
-      isPlaying: false,
-      currentTime: 0,
-    });
-  },
-
-  onTimeUpdate: function(e) {
-    this.setData({
-      currentTime: e.detail.currentTime,
-    });
-  },
+  playMusicById: function (musicId) {
+    const meditation = this.data.musics.find(item => item._id === musicId);
+    if (meditation) {
+      const innerAudioContext = this.data.innerAudioContext;
+      innerAudioContext.stop();
+      innerAudioContext.src = meditation.musicUrl;
+      innerAudioContext.onCanplay(() => {
+        innerAudioContext.play();
+        this.setData({
+          currentMusic: {
+            name: meditation.name,
+            url: meditation.musicUrl,
+            isPlaying: true
+          },
+          currentIndex: this.data.musics.findIndex(item => item._id === musicId)
+        });
+      });
+    } else {
+      console.error('未找到对应的歌曲信息');
+    }
+  }
 });
--- pages/music/music.js
+++ pages/music/music.js
@@ -1,5 +1,4 @@
 const db = wx.cloud.database();
-
 Page({
   data: {
     musics: [],
@@ -9,12 +8,12 @@ Page({
       url: '',
       isPlaying: false
     },
-    currentIndex: 0 // 当前播放音乐的索引
+    currentIndex: 0
   },
   onLoad: function () {
     this.getMusics();
     this.setData({
-      innerAudioContext: wx.createInnerAudioContext(), // 在页面加载时创建 InnerAudioContext 对象
+      innerAudioContext: wx.createInnerAudioContext()
     });
   },
   getMusics: function () {
@@ -29,46 +28,51 @@ Page({
       }
     });
   },
+  onUnload: function () {
+    const innerAudioContext = this.data.innerAudioContext;
+    if (innerAudioContext) {
+      innerAudioContext.stop();
+      innerAudioContext.destroy();
+    }
+  },
   playMusic: function (event) {
     const musicId = event.currentTarget.dataset.id;
     const music = this.data.musics.find(item => item._id === musicId);
     if (music) {
       const innerAudioContext = this.data.innerAudioContext;
-      if (this.data.innerAudioContext) {
-        innerAudioContext.stop();
-      }
+      innerAudioContext.pause();
       innerAudioContext.src = music.musicUrl;
-      innerAudioContext.play();
-      this.setData({
-        currentMusic: {
-          name: music.name,
-          url: music.musicUrl,
-          isPlaying: true
-        },
-        currentIndex: this.data.musics.findIndex(item => item._id === musicId) // 更新当前播放音乐的索引
+      innerAudioContext.onCanplay(() => {
+        innerAudioContext.play();
+        this.setData({
+          currentMusic: {
+            name: music.name,
+            url: music.musicUrl,
+            isPlaying: true
+          },
+          currentIndex: this.data.musics.findIndex(item => item._id === musicId)
+        });
       });
     } else {
       console.error('未找到对应的歌曲信息');
     }
   },
-  pauseMusic: function () {
+  togglePlayPause: function () {
     const innerAudioContext = this.data.innerAudioContext;
-    innerAudioContext.pause();
+    const currentMusic = this.data.currentMusic;
+    if (currentMusic.isPlaying) {
+      innerAudioContext.pause();
+    } else {
+      innerAudioContext.play();
+    }
     this.setData({
-      'currentMusic.isPlaying': false,
-    });
-  },
-  resumeMusic: function () {
-    const innerAudioContext = this.data.innerAudioContext;
-    innerAudioContext.play();
-    this.setData({
-      'currentMusic.isPlaying': true,
+      'currentMusic.isPlaying': !currentMusic.isPlaying,
     });
   },
   prevMusic: function () {
     let index = this.data.currentIndex - 1;
     if (index < 0) {
-      index = this.data.musics.length - 1; // 如果当前是第一首音乐，则切换到最后一首
+      index = this.data.musics.length - 1;
     }
     const music = this.data.musics[index];
     this.playMusicById(music._id);
@@ -76,31 +80,31 @@ Page({
   nextMusic: function () {
     let index = this.data.currentIndex + 1;
     if (index >= this.data.musics.length) {
-      index = 0; // 如果当前是最后一首音乐，则切换到第一首
+      index = 0;
     }
     const music = this.data.musics[index];
     this.playMusicById(music._id);
+
   },
   playMusicById: function (musicId) {
     const music = this.data.musics.find(item => item._id === musicId);
     if (music) {
       const innerAudioContext = this.data.innerAudioContext;
-      if (this.data.innerAudioContext) {
-        innerAudioContext.stop();
-      }
+      innerAudioContext.stop();
       innerAudioContext.src = music.musicUrl;
-      innerAudioContext.play();
-      this.setData({
-        currentMusic: {
-          name: music.name,
-          url: music.musicUrl,
-          isPlaying: true,
-        },
-        currentIndex: this.data.musics.findIndex(item => item._id === musicId) // 更新当前播放音乐的索引
+      innerAudioContext.onCanplay(() => {
+        innerAudioContext.play();
+        this.setData({
+          currentMusic: {
+            name: music.name,
+            url: music.musicUrl,
+            isPlaying: true
+          },
+          currentIndex: this.data.musics.findIndex(item => item._id === musicId)
+        });
       });
     } else {
       console.error('未找到对应的歌曲信息');
     }
   }
-  
 });
